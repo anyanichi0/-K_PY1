98da8e4e658172ee82815c539f85b51f8c411a29
anyanichi0 <115882909+anyanichi0@users.noreply.github.com>
2022-11-13 23:28:11 +0300

    lab3

---
+++ lab3/task1.py
@@ -0,0 +1,9 @@
+src = not False and True or False and not True
+
+#  1. scr = True and True or False and False
+#  2. scr = True or False
+#  3. scr = True  # TODO расписать упрощение выражения
+
+result = src  # TODO подставить результат выражения
+
+print(src == result)
